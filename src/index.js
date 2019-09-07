36986f428c0389282b97b7349be1052a9783a02c
roybarak80 <barakroy@gmail.com>
2019-09-07 11:00:29 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 import { Provider } from 'react-redux';
-import store from './store';
+import { store, history } from './store';
+import { ConnectedRouter } from 'connected-react-router';
+
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
-    </Provider>
-    , document.getElementById('root'));
+        <ConnectedRouter history={history}>
+            <App />
+        </ConnectedRouter>
+    </Provider>,
+    document.getElementById('root'));
--- src/store.js
+++ src/store.js
@@ -1,8 +1,9 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import { createBrowserHistory } from 'history';
 
 import sitesReducer from './reducers/sitesReducer';
-
+export const history = createBrowserHistory();
 const defaultState = {
     defaultCityCode: 215854
 };
